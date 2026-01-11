aa9f9cccae41273ea2b2ac765878b49db275f4d4
Sitesh Mishra <siteshmishra23@gmail.com>
2026-01-11 21:49:45 +0530

    Add files via upload

---
+++ Arrays/leetcode283.cpp
@@ -0,0 +1,17 @@
+// 283. Move Zeroes
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int size=nums.size();
+        int j =0;
+        for(int i =0;i<size;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        
+        
+    }
+};
